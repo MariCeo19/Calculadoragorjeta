787002c272c2bd349cc5e82f9a6a35b479068334
MariCeo19 <marianathayza3@gmail.com>
2023-05-09 17:25:36 -0300

    09/05/2023

--- app/src/main/java/com/example/calculadoragorjeta/MainActivity.kt
+++ app/src/main/java/com/example/calculadoragorjeta/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.calculadoragorjeta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calculadoragorjeta.ui.theme.CalculadoragorjetaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +39,56 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   Calculadora()
                 }
             }
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Calculadora() {
+    var valorentrada by remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculadoragorjetaTheme {
-        Greeting("Android")
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ){
+    }
+
+    Column(
+    verticalArrangement = Arrangement.Top,
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = Modifier
+        .fillMaxSize()
+
+) {
+
+     Text(
+      text = "Calculadora de Gorjeta",
+      fontSize = 30.sp,
+      fontWeight = FontWeight.SemiBold,
+      modifier = Modifier
+          .padding(top = 40.dp)
+  )
+     TextField(
+      value = valorentrada ,
+         label = {
+                 Text(
+                     text = "Valor da entrada" )
+         },
+      onValueChange = {valorentrada = it },
+      modifier = Modifier
+          .padding(top = 30.dp)
+  )
+     Text(
+         text = "Valor da Gorjeta:",
+         fontSize = 25.sp,
+         fontWeight = FontWeight.Normal,
+         modifier = Modifier
+             .padding(top = 30.dp)
+     )
+
     }
 }
