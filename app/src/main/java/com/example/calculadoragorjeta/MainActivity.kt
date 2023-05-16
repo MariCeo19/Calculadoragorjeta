8f89abaf62ad26b52c612e6961cde595697c382c
MariCeo19 <marianathayza3@gmail.com>
2023-05-16 16:47:01 -0300

    16/05/2023 Terça

--- app/src/main/java/com/example/calculadoragorjeta/MainActivity.kt
+++ app/src/main/java/com/example/calculadoragorjeta/MainActivity.kt
@@ -3,12 +3,11 @@ package com.example.calculadoragorjeta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -22,9 +21,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,26 +54,26 @@ class MainActivity : ComponentActivity() {
 fun Calculadora(){
 
     var valorEntrada by remember { mutableStateOf("") }
-    var gorjeta by remember { mutableStateOf(0.0) }
+    var gorjeta = 0.0
     var percentagemGorjeta by remember { mutableStateOf("" ) }
+
+    val focusManager = LocalFocusManager.current
 
     gorjeta = CalcularGorjeta(valorEntrada,percentagemGorjeta)
 
-    Surface (
+    Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
-            )
+           )
     {
     }
-
     Column(
     verticalArrangement = Arrangement.Top,
     horizontalAlignment = Alignment.CenterHorizontally,
-    modifier = Modifier.fillMaxSize()
 
+    modifier = Modifier.fillMaxSize()
           )
     {
-
      Text(
          text = "Calculadora de Gorjeta",
          fontSize = 30.sp,
@@ -81,37 +81,54 @@ fun Calculadora(){
          modifier = Modifier
           .padding(top = 40.dp)
          )
-     TextField(
-         value = valorEntrada,
-         label = {
-             Text(
-                 text = "Valor da entrada" )
-                 },
-         onValueChange = {valorEntrada = it},
-         modifier = Modifier
-          .padding(top = 30.dp),
-         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-
-         TextField(
-             value = valorEntrada,
-             label = {
-                 Text(
-                     text = "Percentagem da Gorjeta" )
-             },
-             onValueChange = {valorEntrada = it},
-             modifier = Modifier
-                 .padding(top = 30.dp),
-             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-
-         ) ,
-         Text(
+        CampoTextoEditavel(
+            value = valorEntrada ,
+            label = "Valor de Entrada" ,
+            onvalueChange = {valorEntrada= it},
+            imeAction = ImeAction.Next,
+            keyboardActions = KeyboardActions (
+                onNext = {focusManager.moveFocus(FocusDirection.Down) }
+            )
+        )
+        CampoTextoEditavel(
+            value = percentagemGorjeta,
+            label = "% Gorjeta",
+            onvalueChange = {percentagemGorjeta= it},
+            imeAction = ImeAction.Done,
+            keyboardActions = KeyboardActions (
+                onNext = {focusManager.clearFocus() }
+            )
+        )
+     Text(
           text = "Valor da Gorjeta ${NumberFormat.getCurrencyInstance().format(gorjeta)}",
          fontSize = 25.sp,
          fontWeight = FontWeight.Normal,
          modifier = Modifier
              .padding(top = 30.dp),
-            )
+         )
     }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CampoTextoEditavel (
+    value: String,
+    label: String,
+    onvalueChange: (String) -> Unit,
+    imeAction: ImeAction,
+    keyboardActions: KeyboardActions
+) {
+      TextField(
+        value = value ,
+        label= {
+            Text(
+                text = label
+                )
+               },
+        onValueChange = onvalueChange,
+        modifier = Modifier
+            .padding(top = 5.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
 }
 
 fun CalcularGorjeta (
